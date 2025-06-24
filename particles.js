cdfe1eb88d10d9c741f00d83e39493d1caea3eb3
tecnico-49 <tecnicoensistemassenatec49@gmail.com>
2025-06-23 22:34:33 -0500

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,62 +1,27 @@
 {
   "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      }
-    },
-    "opacity": {
-      "value": 0.3,
-      "random": true
-    },
-    "size": {
-      "value": 3,
-      "random": true
-    },
+    "number": { "value": 80 },
+    "color": { "value": "#ffffff" },
+    "shape": { "type": "circle" },
+    "opacity": { "value": 0.4 },
+    "size": { "value": 2 },
     "line_linked": {
       "enable": true,
       "distance": 120,
       "color": "#ffffff",
-      "opacity": 0.2,
+      "opacity": 0.3,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 1.5,
+      "speed": 1.3,
       "direction": "none",
-      "random": false,
-      "straight": false,
       "out_mode": "out"
     }
   },
   "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      }
-    },
-    "modes": {
-      "grab": {
-        "distance": 150,
-        "line_linked": {
-          "opacity": 0.35
-        }
-      }
-    }
+    "events": { "onhover": { "enable": true, "mode": "repulse" } },
+    "modes": { "repulse": { "distance": 100 } }
   },
   "retina_detect": true
 }
